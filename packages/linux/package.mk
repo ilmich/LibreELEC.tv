c6718b86779f87326186b33d8a1f735b1dc78654
ilmich <ardutu@gmail.com>
2022-12-15 18:42:16 +0100

    linux: updated to 5.10.158

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -28,8 +28,8 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="5.10.156"
-    PKG_SHA256="679e9964ca720027967391b61db990ceb7868e93e203f87724f18310f4955923"
+    PKG_VERSION="5.10.158"
+    PKG_SHA256="1e0a24bb5510caa18b3601b25e12cc2a1ce123948de551f4f2cdbb40aea707e7"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
