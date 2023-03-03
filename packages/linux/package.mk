db712499c8a0afe401501be8a67dfb67f26ada30
ilmich <ardutu@gmail.com>
2023-03-03 19:40:09 +0100

    kernel: updated to 5.10.170

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -28,8 +28,8 @@ case "${LINUX}" in
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
   *)
-    PKG_VERSION="5.10.161"
-    PKG_SHA256="7aaaf6d0bcd8a2cfa14ad75f02ca62bb2de08aad3bee3eff198de49ea5254079"
+    PKG_VERSION="5.10.170"
+    PKG_SHA256="a2b51876befb8cc35724ed62820845f2b387d471a6cf46e8eedd0b6cb595825f"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/${PKG_NAME}-${PKG_VERSION}.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
