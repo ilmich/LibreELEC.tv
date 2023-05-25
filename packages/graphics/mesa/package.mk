2e499da94f6cfe39dbfe025a9d91feb664b5f215
ilmich <ardutu@gmail.com>
2023-05-25 13:10:43 +0200

    mesa: upgraded to 23.0.3

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="23.0.1"
-PKG_SHA256="e8e586856b55893abae9bdcdb98b41c081d909bb1faf372e6e7262307bf34adf"
+PKG_VERSION="23.0.3"
+PKG_SHA256="386362a5d80df3b096636b67f340e1ce67b705b44767d5bdd11d2ed1037192d5"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
